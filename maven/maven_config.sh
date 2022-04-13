8ae6a198f414155ab9a797217cfa8842ae1c3a40
yoshida.shinya <yoshida.shinya@linecorp.com>
2022-04-13 21:20:56 +0900

    Support multi config for maven

--- maven/maven_config.sh
+++ maven/maven_config.sh
@@ -7,9 +7,11 @@ if [ ${FZFLET_MAVEN_CONFIG:-0} -eq 0 ]; then
     . $basedir/default.rc
     load_config
 
-    export FZFLET_MAVEN_BATCH_SIZE=${FZFLET_MAVEN_BATCH_SIZE:-${FZFLET_MAVEN_DEFAULT_BATCH_SIZE:-50}}
+    export FZFLET_MAVEN_CONTEXT=${FZFLET_MAVEN_CONTEXT:-${FZFLET_MAVEN_DEFAULT_CONTEXT:-DEFAULT}}
 
-    url=${FZFLET_MAVEN_URL:-${FZFLET_MAVEN_DEFAULT_URL:-https://search.maven.org/}}
+    export FZFLET_MAVEN_BATCH_SIZE=${FZFLET_MAVEN_BATCH_SIZE:-$(eval echo "\${FZFLET_MAVEN_${FZFLET_MAVEN_CONTEXT}_BATCH_SIZE:-50}")}
+
+    url=${FZFLET_MAVEN_URL:-$(eval echo "\${FZFLET_MAVEN_${FZFLET_MAVEN_CONTEXT}_URL:-https://search.maven.org/}")}
     while [ -z "$url" ]; do
         echo -n "Input url> "
         read url
@@ -19,21 +21,21 @@ if [ ${FZFLET_MAVEN_CONFIG:-0} -eq 0 ]; then
         fi
     done
     export FZFLET_MAVEN_URL=$url
-    export FZFLET_MAVEN_EXTRA_QUERY=${FZFLET_MAVEN_EXTRA_QUERY:-${FZFLET_MAVEN_DEFAULT_EXTRA_QUERY:-}}
+    export FZFLET_MAVEN_EXTRA_QUERY=${FZFLET_MAVEN_EXTRA_QUERY:-$(eval echo "\${FZFLET_MAVEN_${FZFLET_MAVEN_CONTEXT}_EXTRA_QUERY:-}")}
 
     if [ -z "${FZFLET_MAVEN_OPEN_URL+1}" ]; then
-        export FZFLET_MAVEN_OPEN_URL=${FZFLET_MAVEN_DEFAULT_OPEN_URL:-'https://mvnrepository.com/artifact/{1}/{2}/{3}'}
+        export FZFLET_MAVEN_OPEN_URL=$(eval echo "\${FZFLET_MAVEN_${FZFLET_MAVEN_CONTEXT}_OPEN_URL:-'https://mvnrepository.com/artifact/{1}/{2}/{3}'}")
     fi
 
     if [ -z "${FZFLET_MAVEN_AUTH+1}" ]; then
-        export FZFLET_MAVEN_AUTH=$FZFLET_MAVEN_DEFAULT_AUTH
+        export FZFLET_MAVEN_AUTH=$(eval echo "\${FZFLET_MAVEN_${FZFLET_MAVEN_CONTEXT}_AUTH}")
     fi
     case $FZFLET_MAVEN_AUTH in
         "basic" )
             if [ -n "${FZFLET_MAVEN_USER+1}" ]; then
                 user=$FZFLET_MAVEN_USER
             else
-                user=$FZFLET_MAVEN_DEFAULT_USER
+                user=$(eval echo "\${FZFLET_MAVEN_${FZFLET_MAVEN_CONTEXT}_USER}")
             fi
             while [ -z "$user" ]; do
                 echo -n "Input username for $url> "
@@ -48,7 +50,7 @@ if [ ${FZFLET_MAVEN_CONFIG:-0} -eq 0 ]; then
             if [ -n "${FZFLET_MAVEN_PASSWORD+1}" ]; then
                 password=$FZFLET_MAVEN_PASSWORD
             else
-                password=$FZFLET_MAVEN_DEFAULT_PASSWORD
+                password=$(eval echo "\${FZFLET_MAVEN_${FZFLET_MAVEN_CONTEXT}_PASSWORD}")
             fi
             while [ -z "$password" ]; do
                 echo -n "Input password for $url> "
@@ -62,8 +64,8 @@ if [ ${FZFLET_MAVEN_CONFIG:-0} -eq 0 ]; then
             ;;
     esac
 
-    export FZFLET_MAVEN_SEARCH=${FZFLET_MAVEN_SEARCH:-${FZFLET_MAVEN_DEFAULT_SEARCH:-maven_search.sh}}
-    export FZFLET_MAVEN_PREVIEW=${FZFLET_MAVEN_PREVIEW:-${FZFLET_MAVEN_DEFAULT_PREVIEW:-maven_preview.sh}}
+    export FZFLET_MAVEN_SEARCH=${FZFLET_MAVEN_SEARCH:-$(eval echo "\${FZFLET_MAVEN_${FZFLET_MAVEN_CONTEXT}_SEARCH:-maven_search.sh}")}
+    export FZFLET_MAVEN_PREVIEW=${FZFLET_MAVEN_PREVIEW:-$(eval echo "\${FZFLET_MAVEN_${FZFLET_MAVEN_CONTEXT}_PREVIEW:-maven_preview.sh}")}
 
     export FZFLET_MAVEN_CONFIG=1
 fi
