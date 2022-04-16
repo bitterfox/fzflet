ff42b08c5291a50f43a2433c101c1f105741507d
yoshida.shinya <yoshida.shinya@linecorp.com>
2022-04-16 19:37:16 +0900

    Use toggle prompt for ag

--- ag/fzf_ag.sh
+++ ag/fzf_ag.sh
@@ -11,8 +11,9 @@ FZF_DEFAULT_OPTS="--reverse --height ${FZF_TMUX_HEIGHT:-96%} $FZF_DEFAULT_OPTS"
 $basedir/ag_reload.sh $path 2>/dev/null |
     $(__fzfcmd) -d : \
                 --header-lines=1 \
+                --prompt "Ag: " \
                 --bind "change:reload($basedir/ag_reload.sh $path {q})" \
-                --bind 'ctrl-r:clear-query+toggle-search+toggle-reload' \
+                --bind 'ctrl-r:clear-query+toggle-search+toggle-reload+toggle-prompt(Fzf: )' \
                 --preview "$basedir/../preview/cat.sh {1} {2}" \
                 --preview-window down:+{2}-/2 \
                 --disabled -m "$@"
