ee1476eea7684e46093d7ee49346e3fdf0c886f2
yoshida.shinya <yoshida.shinya@linecorp.com>
2022-04-21 22:34:20 +0900

    Fix zsh history

--- zsh/zsh_history.sh
+++ zsh/zsh_history.sh
@@ -4,4 +4,4 @@ HISTFILE=~/.zsh_history
 HISTSIZE=999999999
 fc -R
 
-fc -rl 1 | sort -r -k 2 | uniq -f 1 | sort -r -n -k 1 | cut -d' ' -f2-
+fc -rl 1 | sort -r -k 2 | uniq -f 1 | sort -r -n -k 1 | cut -d' ' -f3-
