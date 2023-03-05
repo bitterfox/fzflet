cd457337143de3ffa19426e0200f0762f507eb22
yoshida.shinya <yoshida.shinya@linecorp.com>
2023-03-05 13:07:26 +0900

    Improve fzf jira list issue

--- jira/fzf_jira_list_issues.sh
+++ jira/fzf_jira_list_issues.sh
@@ -12,10 +12,17 @@ fzf_jira_list_issues() {
         option="$option"$'\t'"--preview"$'\t'"$basedir/jira_preview_issue.sh {1}"
     fi
     IFS=$'\t'
+
+    header=$(
+        echo "Enter          M-Enter        C-o"
+        echo "Print Jira Id  Print Jira ID  Open in browser"
+    )
     FZF_DEFAULT_COMMAND=$basedir/jira_list_issues.sh \
         fzf $option \
+        --header "$header" \
+        --expect enter,alt-enter \
         --bind "ctrl-o:execute-silent(xdg-open $FZFLET_JIRA_URL/browse/{1})" \
-        --expect enter,alt-m
+        # --header "`print "Enter: Print Jira ID, M-Enter: Print Jira ID and title, \nC-o: Open in browser"`" \
         # --bind "ctrl-l:execute-and-exit-on-success($FZFLET_JIRA_PREVIEW | less < /dev/tty > /dev/tty)" \
         # --bind "ctrl-g:reload(echo)+abort"
 }
@@ -28,6 +35,6 @@ if [ $? -eq 0 ]; then
 
     case "$key" in
         "enter" ) echo "$lines" | awk '{print $1}' ;;
-        "alt-m" ) echo "$lines" ;;
+        "alt-enter" ) echo "$lines" ;;
     esac
 fi
