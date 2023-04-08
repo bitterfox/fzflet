6d0addb46312e5bdf9dda513c07412fed7cf1b5f
yoshida.shinya <yoshida.shinya@linecorp.com>
2023-04-08 22:21:03 +0900

    Skip load config if no wip task to preview

--- jira/jira_worklog_preview_task.sh
+++ jira/jira_worklog_preview_task.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-basedir=`dirname $0`
-. $basedir/../util/common.sh
-
-. $basedir/jira_config.sh
-
 if [ -f ~/.wip_task ] && [ -s ~/.wip_task ]; then
+    basedir=`dirname $0`
+    . $basedir/../util/common.sh
+
+    . $basedir/jira_config.sh
+
     task=`cat ~/.wip_task | head -n 1`
     start_time=`cat ~/.wip_task | head -n 2 | tail -n 1`
     elapsed_time=`$basedir/jira_worklog_format_elapsed_time.sh $start_time`
