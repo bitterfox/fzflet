b67d30faa8a7a74f5e50e4e8dcac81a9fc6048ee
yoshida.shinya <yoshida.shinya@linecorp.com>
2024-11-16 10:00:59 +0900

    Support files open by editor, IDE

--- files/fzf_files_find.sh
+++ files/fzf_files_find.sh
@@ -9,6 +9,9 @@ if [ $# -ge 1 ]; then
   shift
 fi
 
+. $basedir/../util/common.sh
+load_config
+
 FZFLET_FILES_FIND_TARGET="${FZFLET_FILES_FIND_TARGET:-file}"
 case "$FZFLET_FILES_FIND_TARGET" in
     "file") NEXT_FZFLET_FILES_FIND_TARGET="dir" ;;
@@ -23,5 +26,5 @@ $basedir/files_find.sh "$path" | fzf \
   --query="$query" \
   --bind "ctrl-t:execute-and-exit-on-success(FZFLET_FILES_FIND_TARGET=$NEXT_FZFLET_FILES_FIND_TARGET $0 \"$path\" {q} || exit 0)" \
   --preview="$basedir/../preview/preview_path.sh $path/{}" \
-  --bind "ctrl-o:execute-silent(emacs $path/{} &)" \
-  --bind "alt-o:execute-silent(emacs $path/{} &)"
+  --bind "ctrl-o:execute-silent($FZFLET_FILE_OPEN_EDITOR)" \
+  --bind "alt-o:execute-silent($FZFLET_FILE_OPEN_IDE)"
